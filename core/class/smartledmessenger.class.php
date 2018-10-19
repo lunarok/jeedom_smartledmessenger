12fd402defcdfbd7f8a83e874c60cc2d9dc2e5a7
Cédric Dupont <lunarok@gmail.com>
2018-10-19 14:48:11 +0200

    first version

---
+++ core/class/smartledmessenger.class.php
@@ -0,0 +1,84 @@
+<?php
+
+/* This file is part of Jeedom.
+*
+* Jeedom is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Jeedom is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* * ***************************Includes********************************* */
+require_once dirname(__FILE__) . '/../../../../core/php/core.inc.php';
+
+class smartledmessenger extends eqLogic {
+
+	public function loadCmdFromConf($type) {
+		if ($type == 'geofence') {
+			return true;
+		}
+		if (!is_file(dirname(__FILE__) . '/../config/devices/' . $type . '.json')) {
+			return;
+		}
+		$content = file_get_contents(dirname(__FILE__) . '/../config/devices/' . $type . '.json');
+		if (!is_json($content)) {
+			return;
+		}
+		$device = json_decode($content, true);
+		if (!is_array($device) || !isset($device['commands'])) {
+			return true;
+		}
+		foreach ($device['commands'] as $command) {
+			$cmd = null;
+			foreach ($this->getCmd() as $liste_cmd) {
+				if ((isset($command['logicalId']) && $liste_cmd->getLogicalId() == $command['logicalId'])
+				|| (isset($command['name']) && $liste_cmd->getName() == $command['name'])) {
+					$cmd = $liste_cmd;
+					break;
+				}
+			}
+			if ($cmd == null || !is_object($cmd)) {
+				$cmd = new smartledmessengerCmd();
+				$cmd->setEqLogic_id($this->getId());
+				utils::a2o($cmd, $command);
+				$cmd->save();
+			}
+		}
+	}
+
+	public function postAjax() {
+		$this->loadCmdFromConf($this->getConfiguration('type'));
+	}
+
+	public function sendMessage($_options = array()) {
+		if ($_options['message'] == '') {
+			return;
+		}
+		if (isset($_options['title'])) {
+			$options = arg2array($_options['title']);
+		}
+		$intensity = (isset($options['intensity'])) ? $options['intensity'] : $this->getConfiguration('intensity'); // 0 à 15
+		$speed = (isset($options['speed'])) ? $options['speed'] : $this->getConfiguration('speed'); // 10 à 50
+		$static = (isset($options['static'])) ? $options['static'] : $this->getConfiguration('static'); // binaire
+		$url = 'http://' . $this->getConfiguration('addr') . '/?message=' . urlencode($_options['message']) . '&intensity=' . $intensity . '&speed=' . $speed . '&local=1&static=' . $static;
+		$request_http = new com_http($url);
+		$data = $request_http->exec(30);
+	}
+
+}
+
+class smartledmessengerCmd extends cmd {
+	public function execute($_options = array()) {
+		$eqLogic = $this->getEqLogic();
+		$eqLogic->sendMessage($_options);
+	}
+}
+?>
---
+++ desktop/js/smartledmessenger.js
@@ -0,0 +1,61 @@
+
+/* This file is part of Jeedom.
+ *
+ * Jeedom is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jeedom is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
+ */
+ $('#cmdgeoloc').on('click', function () {
+    jeedom.cmd.getSelectModal({cmd: {type: 'info', subType: 'string'}}, function (result) {
+        $('.eqLogicAttr[data-l2key=cmdgeoloc]').value(result.human);
+    });
+});
+
+ $("#table_cmd").sortable({axis: "y", cursor: "move", items: ".cmd", placeholder: "ui-state-highlight", tolerance: "intersect", forcePlaceholderSize: true});
+
+function addCmdToTable(_cmd) {
+    if (!isset(_cmd)) {
+        var _cmd = {configuration: {}};
+    }
+
+    var tr = '<tr class="cmd" data-cmd_id="' + init(_cmd.id) + '">';
+    tr += '<td>';
+    tr += '<span class="cmdAttr" data-l1key="id" ></span>';
+    tr += '</td>';
+    tr += '<td>';
+    tr += '<input class="cmdAttr form-control input-sm" data-l1key="name" >';
+    tr += '</td>';
+    tr += '<td>';
+    tr += '<span class="cmdAttr" data-l1key="type"></span>';
+    tr += '<br/>';
+    tr += '<span class="cmdAttr" data-l1key="subType"></span>';
+    tr += '</td>';
+    tr += '<td>';
+    if (init(_cmd.subType) == 'numeric' || init(_cmd.subType) == 'binary') {
+        tr += '<label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isHistorized" data-size="mini" checked/>{{Historiser}}</label></span> ';
+    }
+    if (init(_cmd.configuration.type) == 'geofence') {
+      tr += '<span><label class="checkbox-inline"><input type="checkbox" class="cmdAttr checkbox-inline" data-l1key="isVisible" data-size="mini" checked/>{{Afficher}}</label></span> ';
+    }
+    tr += '</td>';
+    tr += '<td>';
+    if (is_numeric(_cmd.id)) {
+        tr += '<a class="btn btn-default btn-xs cmdAction" data-action="configure"><i class="fa fa-cogs"></i></a> ';
+    }
+   if (init(_cmd.type) == 'info') {
+        tr += '<a class="btn btn-default btn-xs cmdAction" data-action="test"><i class="fa fa-rss"></i> {{Tester}}</a>';
+    }
+    tr += '<i class="fa fa-minus-circle pull-right cmdAction cursor" data-action="remove"></i></td>';
+    tr += '</tr>';
+    $('#table_cmd tbody').append(tr);
+    $('#table_cmd tbody tr:last').setValues(_cmd, '.cmdAttr');
+}
