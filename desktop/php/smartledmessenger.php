713bfc46bc427ddc3ef268a8b45cba64c79dc072
Cédric Dupont <lunarok@gmail.com>
2018-11-30 15:37:12 +0100

    matrix display

--- desktop/php/smartledmessenger.php
+++ desktop/php/smartledmessenger.php
@@ -22,7 +22,7 @@ $eqLogics = eqLogic::byType('smartledmessenger');
 	</div>
 
 	<div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
-		<legend><i class="icon nature-planet5"></i> {{Mes Smart Led Messenger}}</legend>
+		<legend><i class="icon nature-planet5"></i> {{Mes Matrix Display}}</legend>
 		<div class="eqLogicThumbnailContainer">
 			<div class="cursor eqLogicAction" data-action="add" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >
 				<center>
